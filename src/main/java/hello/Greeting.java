4838ee8a06f02d891677187b79b5a4b46509d8ed
Peter Wildberger <pete.wildberger@gmail.com>
2017-07-25 20:23:11 -0500

    init commit

---
+++ src/main/java/hello/Greeting.java
@@ -0,0 +1,24 @@
+package hello;
+
+public class Greeting {
+
+    private final long id;
+    private final String content;
+
+    public Greeting(long id, String name, int age, ) {
+        this.id = id;
+        this.content = content;
+        this.age = age;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return content;
+    }
+    public int getName() {
+        return age;
+    }
+}
